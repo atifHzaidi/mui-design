a6fdb1e548d71e783e3771a6c23b2ed2aef58b83
Syed Zaman <93176941+syedzamanhasnain@users.noreply.github.com>
2024-01-13 13:22:56 +0530

    added theme

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,16 @@
 import type { Preview } from "@storybook/react";
 
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+
+/* TODO: update import for your custom Material UI themes */
+import { lightTheme, darkTheme } from "../path/to/themes";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -10,6 +21,19 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
